11e3bac5df5449780f65a04eae3f5f48ab923b5a
Hubert Dryja <hubertdryja@gmail.com>
2019-05-20 22:31:30 +0200

    init

---
+++ generate_map.py
@@ -0,0 +1,17 @@
+import sys
+import random
+
+import numpy as np
+
+
+def random_map(n):
+    return np.random.randint(1000, size=(n, n))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Use: {} file_output number_of_cites".format(sys.argv[0]))
+    else:
+        n = int(sys.argv[2])
+        mat = random_map(n)
+        np.savetxt(sys.argv[1], mat, fmt="%d", header=str(n), comments="")
